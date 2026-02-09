95865f54232554ea8dc6f3f30547dc492ee73586
mohamedwalaaeldeenonsor <mohamed.walaaeldeen@onsor.om>
2026-02-09 16:44:00 +0200

    fix: remove incorrect import and ensure accessToken is included in query parameters for demo request

--- lib/src/service/demo_service.dart
+++ lib/src/service/demo_service.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:thingsboard_client/src/model/demo/demo_request_paylaod.dart';
-import 'package:thingsboard_client/src/model/demo/demo_request_response.dart';
 import 'package:thingsboard_client/thingsboard_client.dart';
 
 class DemoService {
@@ -18,6 +17,7 @@ class DemoService {
     var response = await _tbClient.post<Map<String, dynamic>>(
         '/api/demo/request',
         data: jsonEncode(demoRequestPayload),
+        queryParameters: {'accessToken': accessToken},
         options: defaultHttpOptionsFromConfig(requestConfig));
     return DemoRequestResponse.fromJson(response.data!);
   }
